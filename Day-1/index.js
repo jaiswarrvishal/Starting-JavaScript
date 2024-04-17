238e942cd1580f560e8f373287a7f1a0ee3ec40f
jaiswarrvishal <163637594+jaiswarrvishal@users.noreply.github.com>
2024-04-17 20:07:14 +0530

    yesterday's update

---
+++ Day-1/index.js
@@ -0,0 +1,14 @@
+// Data-> it could be anything like: name/number/boolean/undefined(basically data is an information)
+// Datatypes-> 1.String, 2.numbaer, 3.boolean, 4.undefined
+// 1. String-> "Vishal"(data should be in '' or "" or ``) 2. Numaber-> 28(any number) 3. boolean-> true/false 4. undefined->not defined
+
+// Variable:-> we can store the data in variable.
+// Three types of variables are ther 1.var 2.let 3.const
+var name = "Vishal";
+var age = 24
+var assignmentCompleted = true; 
+
+//alert() and console.log() --> it is a preDefined function
+console.log(name);
+console.log(age);
+console.log(assignmentCompleted);
