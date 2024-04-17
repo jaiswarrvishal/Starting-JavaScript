29f582ba0475b4f622ab4518c3a6a7d670ad0c6b
jaiswarrvishal <163637594+jaiswarrvishal@users.noreply.github.com>
2024-04-17 21:12:53 +0530

    today's update

---
+++ Day-2/index.js
@@ -0,0 +1,38 @@
+console.log(age); //this will give "undefined" 
+var age=20;
+console.log(age);
+age=30;
+console.log(age);
+age=28;
+
+console.log(age);
+//JS print hte value line by line
+
+//1. Defference between: var,let, const
+//2. Defference between: Scope
+//3. Defference between: Hoisnting
+
+//1. Defference between: var,let, const
+var name = "vishal";
+console.log(name); 
+ name = "vishal"; //possible-->re-assigned
+var name = "vishal"; //possibe --> declared
+
+
+let a =11;
+console.log(a);
+// a = 13; // possible-->re-assigned 
+// let a = 12; // not possibe --> declared
+
+const num = 100;
+console.log(num);
+// num = 111; // not possible-->re-assigned
+// const num =112; // not possibe --> declared
+
+let number = 40;
+let number1 = 5;
+console.log(number + number1);
+console.log(number - number1);
+console.log(number * number1);
+console.log(number / number1);
+console.log(number % number1);
