f5d0d97a4f86544a3ff9a7d1b1b9369ce6d65923
jaiswarrvishal <163637594+jaiswarrvishal@users.noreply.github.com>
2024-05-01 12:07:02 +0530

    updates

---
+++ Day-9/index.js
@@ -0,0 +1,20 @@
+// Q. given an array, find two numbers whose addtion is target.
+var array = [1, 2, 3, 4, 5, 6];
+var target = 5;
+
+// var count = 0;
+
+console.log("the two numbers whose addition is target")
+for(var i = 0; i<array.length; i++){
+    // for(var j = 0; j<array.length; j++){ // count => 36
+    for(var j = i + 1; j<array.length; j++){ // count => 15
+        // console.log("target");
+        // count++;
+        // console.log(array[i])
+        // console.log(array[i], "+", array[j], "=", array[i] + array[j]);
+        if(array[i] + array[j] === target){
+            console.log(array[i], array[j])
+        }
+    }
+}
+// console.log("count", count)
