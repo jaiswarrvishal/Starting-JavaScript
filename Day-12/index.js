b4e816c20ace597301c82472c05885f34a643d73
jaiswarrvishal <163637594+jaiswarrvishal@users.noreply.github.com>
2024-05-08 22:07:21 +0530

    update

---
+++ Day-12/index.js
@@ -0,0 +1,63 @@
+// till we covered few tpics in JS
+
+//DataType: number/string/undefined/boolean
+
+//Variables(to store the datas): var/let/const
+//Re-assign and Re-Declared: 
+                        // in var=> both possible
+                        // in let=> Re-assign is possible but Re-declared not possible
+                        // in const=> both not possible
+
+// scope and HOISTING
+
+// Conditional statement: if-else/switch case
+
+// loops: for and while(pending)
+//for loop: syntax =>
+// for(starting: ending: sequence){
+//     code
+// }
+
+// Comparision Operator
+// > < <= >= != 
+// = == === [=: to assign value, == : to cpmare the data, ===: to compare the data and datatype]
+// logic: || && => 
+// || => or => F||F=>F rest True 
+// && => and => T&&T=>T rest False
+
+// function:
+// Traditional
+// Arrow
+
+
+// Array
+// []
+// Push, pop, slice, splice, shift, unshift
+// Map , foreach , reduce - pending topics ***
+
+// Nested For loop;
+// for(starting: ending: sequence){
+//     for(starting: ending: sequence){
+//         for(s: e: s){
+//              codes
+//         }
+//     }
+// }
+
+
+// Object {key:value}
+// syntax
+// access
+// loop 
+// array - object -> assign - pending topic
+
+//DOM: document.getelementedById("name")  
+
+var array = [1];
+array[2] = 2;
+console.log(array);
+
+var myObject = { name: "Awdiz" };
+myObject["Age"] = 10;
+console.log(myObject);
+
